99587480a4ce94fef64f5f84d7d6d407113a9f5f
johns <johnsun2145@outlook.com>
2024-06-26 10:15:54 +0800

    HttpResult 请求结果对象

---
+++ crm-server/src/main/java/pers/johns/crm/mode/vo/HttpResult.java
@@ -0,0 +1,63 @@
+package pers.johns.crm.mode.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * ClassName    : HttpResult
+ * <br/>
+ * Description  : 用于封装 HTTP 请求结果信息
+ * <br/>
+ * CreateTime   : 2024/6/26 10:01
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class HttpResult implements Serializable {
+    /**
+     * 请求处理后的结果码
+     * 200 = 正常响应
+     * 500 = 错误响应
+     */
+    private Integer code;
+    /**
+     * 请求处理的简要信息
+     */
+    private String msg;
+    /**
+     * 携带数据
+     */
+    private Object data;
+
+    public static HttpResult OK() {
+        return OK(HttpResultCode.OK.getMsg());
+    }
+
+    public static HttpResult OK(String msg) {
+        return HttpResult.builder()
+                .code(HttpResultCode.OK.getCode())
+                .msg(msg)
+                .build();
+    }
+
+    public static HttpResult Fail() {
+        return Fail(HttpResultCode.FAIL.getMsg());
+    }
+
+    public static HttpResult Fail(String msg) {
+        return HttpResult.builder()
+                .code(HttpResultCode.FAIL.getCode())
+                .msg(msg)
+                .build();
+    }
+}
---
+++ crm-server/src/main/java/pers/johns/crm/mode/vo/HttpResultCode.java
@@ -0,0 +1,28 @@
+package pers.johns.crm.mode.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * EnumName     : HttpResultCode
+ * <br/>
+ * EnumValue    :
+ * <br/>
+ * Description  : 用于 HTTP 请求结果的默认值
+ * <br/>
+ * CreateTime   : 2024/6/26 10:04
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+
+@Getter
+@AllArgsConstructor
+public enum HttpResultCode {
+    OK(200, "请求成功"),
+    FAIL(500, "请求失败");
+
+    private final Integer code;
+    private final String msg;
+}
