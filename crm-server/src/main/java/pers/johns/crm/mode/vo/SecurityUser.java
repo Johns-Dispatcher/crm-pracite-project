f6cbe9f1e96861dc80497e79aae54b2a442ccb19
johns <johnsun2145@outlook.com>
2024-06-26 11:42:39 +0800

    查询用户信息用于构建安全用户对象

---
+++ crm-server/src/main/java/pers/johns/crm/mode/vo/SecurityUser.java
@@ -0,0 +1,76 @@
+package pers.johns.crm.mode.vo;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import pers.johns.crm.mode.po.Permission;
+import pers.johns.crm.mode.po.Role;
+import pers.johns.crm.mode.po.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * ClassName    : SecurityUser
+ * <br/>
+ * Description  : 安全用户对象，存储从数据库获取的用户对象，同时擦除密码
+ * <br/>
+ * CreateTime   : 2024/6/26 10:23
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+
+public record SecurityUser(User user) implements UserDetails, Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(Role::getPermissions)
+                .flatMap(List::stream)
+                .map(Permission::getCode)
+                // 去空 权限为空会报错
+                .filter(s -> s != null && !s.isEmpty())
+                .map(SimpleGrantedAuthority::new)
+                .toList();
+    }
+
+    @Override
+    public String getPassword() {
+        String loginPwd = user.getLoginPwd();
+        user.setLoginPwd(null);
+        return loginPwd;
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getLoginAct();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return user.getAccountNoExpired() == 1;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return user.getAccountNoLocked() == 1;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return user.getCredentialsNoExpired() == 1;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.getAccountEnabled() == 1;
+    }
+}
