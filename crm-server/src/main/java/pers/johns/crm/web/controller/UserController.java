0bf418b6a80bb6ce32d7e8556738ca893d2b982d
johns <johnsun2145@outlook.com>
2024-06-28 20:05:55 +0800

    新增 /api/user 接口

--- crm-server/src/main/java/pers/johns/crm/web/controller/UserController.java
+++ crm-server/src/main/java/pers/johns/crm/web/controller/UserController.java
@@ -25,36 +25,9 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class UserController {
 
-    private final UserService userService;
+    @GetMapping("/api/user")
+    public HttpResult getUsers(Authentication authentication) {
 
-    /**
-     * 获取认证信息
-     * @param authentication 用户认证信息
-     * @return 携带有用户信息的 {@link HttpResult} 对象
-     */
-    @GetMapping("/api/login/info")
-    public HttpResult loginInfo(Authentication authentication) {
-        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
-        return HttpResult.OK(securityUser);
-    }
-
-    /**
-     * 免登录接口，能处理到这一定是通过了认证
-     * @return 携带有登录成功信息的 {@link HttpResult} 对象
-     */
-    @GetMapping("/api/login/free")
-    public HttpResult loginFree() {
-        return HttpResult.OK("免登录成功");
-    }
-
-    /**
-     * 续期接口，用于前端用户进行续签，能处理到这一定是通过了认证
-     * @param authentication 用户认证信息
-     * @return 携带有续签信息的 {@link HttpResult} 对象
-     */
-    @GetMapping("/api/login/renewal")
-    public HttpResult loginRenewal(Authentication authentication) {
-        Map<String, Object> data = userService.renewalToken(authentication);
-        return HttpResult.OK("续签成功", data);
+        return HttpResult.OK();
     }
 }
