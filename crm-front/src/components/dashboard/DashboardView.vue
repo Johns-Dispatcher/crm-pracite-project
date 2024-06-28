6a021fef2c11197389b63ec58467451206b98d12
johns <johnsun2145@outlook.com>
2024-06-28 20:04:55 +0800

    整理代码，添加路由跳转

--- crm-front/src/components/dashboard/DashboardView.vue
+++ crm-front/src/components/dashboard/DashboardView.vue
@@ -1,190 +1,178 @@
 <template>
-  <el-container>
-    <!-- 左侧 -->
-    <el-aside :width="asideWidth">
+	<el-container>
+		<!-- 左侧 -->
+		<el-aside :width="asideWidth">
 
-      <div class="menu-title">
-        @CRM管理系统
-      </div>
+			<div class="menu-title">@CRM管理系统</div>
 
-      <!-- 菜单组 -->
-      <el-menu
-          active-text-color="#ff8080"
-          background-color="#005a99"
-          text-color="#fff"
-          :unique-opened="true"
-          :collapse="menuFolded"
-          :collapse-transition="menuTransition"
-      >
-        <!-- 菜单项 市场活动 -->
-        <el-sub-menu index="1">
-          <template #title>
-            <el-icon>
-              <OfficeBuilding/>
-            </el-icon>
-            <span>市场活动</span>
-          </template>
-          <el-menu-item index="1-1">
-            <el-icon>
-              <Suitcase/>
-            </el-icon>
-            市场活动
-          </el-menu-item>
-          <el-menu-item index="1-2">
-            <el-icon>
-              <TrendCharts/>
-            </el-icon>
-            市场统计
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 线索管理 -->
-        <el-sub-menu index="2">
-          <template #title>
-            <el-icon>
-              <DataLine/>
-            </el-icon>
-            <span>线索管理</span>
-          </template>
-          <el-menu-item index="2-1">
-            <el-icon>
-              <DataBoard/>
-            </el-icon>
-            线索管理
-          </el-menu-item>
-          <el-menu-item index="2-2">
-            <el-icon>
-              <DataAnalysis/>
-            </el-icon>
-            线索统计
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 客户管理 -->
-        <el-sub-menu index="3">
-          <template #title>
-            <el-icon>
-              <UserFilled/>
-            </el-icon>
-            <span>客户管理</span>
-          </template>
-          <el-menu-item index="3-1">
-            <el-icon>
-              <List/>
-            </el-icon>
-            客户管理
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 交易管理 -->
-        <el-sub-menu index="4">
-          <template #title>
-            <el-icon>
-              <WalletFilled/>
-            </el-icon>
-            <span>交易管理</span>
-          </template>
-          <el-menu-item index="4-1">
-            <el-icon>
-              <Present/>
-            </el-icon>
-            交易管理
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 产品管理 -->
-        <el-sub-menu index="5">
-          <template #title>
-            <el-icon>
-              <Box/>
-            </el-icon>
-            <span>产品管理</span>
-          </template>
-          <el-menu-item index="5-1">
-            <el-icon>
-              <Goods/>
-            </el-icon>
-            产品管理
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 字典管理 -->
-        <el-sub-menu index="6">
-          <template #title>
-            <el-icon>
-              <Notebook/>
-            </el-icon>
-            <span>字典管理</span>
-          </template>
-          <el-menu-item index="6-1">
-            <el-icon>
-              <Memo/>
-            </el-icon>
-            字典管理
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 用户管理 -->
-        <el-sub-menu index="7">
-          <template #title>
-            <el-icon>
-              <User/>
-            </el-icon>
-            <span>用户管理</span>
-          </template>
-          <el-menu-item index="7-1">
-            <el-icon>
-              <Document/>
-            </el-icon>
-            用户管理
-          </el-menu-item>
-        </el-sub-menu>
-        <!-- 菜单项 系统管理 -->
-        <el-sub-menu index="8">
-          <template #title>
-            <el-icon>
-              <Setting/>
-            </el-icon>
-            <span>系统管理</span>
-          </template>
-          <el-menu-item index="8-1">
-            <el-icon>
-              <Operation/>
-            </el-icon>
-            系统管理
-          </el-menu-item>
-        </el-sub-menu>
-      </el-menu>
-    </el-aside>
+			<!-- 菜单组 -->
+			<el-menu
+				active-text-color="#ff8080"
+				background-color="#005a99"
+				text-color="#fff"
+				:unique-opened="true"
+				:collapse="menuFolded"
+				:collapse-transition="menuTransition"
+				:router="true"
+			>
+			<!-- 菜单项 市场活动 -->
+				<el-sub-menu index="1">
+					
+					<template #title>
+						<el-icon><OfficeBuilding/></el-icon>
+						<span>市场活动</span>
+					</template>
 
-    <!-- 右侧 -->
-    <el-container class="right-container">
-      <!-- 头部 -->
-      <el-header>
-        <el-icon class="fold-icon" @click="changeMenuFolded">
-          <Fold v-if="!menuFolded"/>
-          <Expand v-if="menuFolded"/>
-        </el-icon>
+					<el-menu-item index="1-1">
+						<el-icon><Suitcase/></el-icon>市场活动
+					</el-menu-item>
+					<el-menu-item index="1-2">
+						<el-icon><TrendCharts/></el-icon>市场统计
+					</el-menu-item>
 
-		<div class="dropdown">
-			<el-dropdown :hide-on-click="false" @visible-change="visibleChange">
-				<span class="el-dropdown-link">
-					{{ username }}
-					<el-icon class="el-icon--right" style="vertical-align: middle">
-						<ArrowUp v-if="dropboxVisiable"/>
-						<ArrowDown v-if="!dropboxVisiable" />
-					</el-icon>
-				</span>
-				<template #dropdown>
-					<el-dropdown-menu>
-						<el-dropdown-item>我的资料</el-dropdown-item>
-						<el-dropdown-item>修改密码</el-dropdown-item>
-						<el-dropdown-item divided @click="logout">退出登录</el-dropdown-item>
-					</el-dropdown-menu>
-				</template>
-			</el-dropdown>
-		</div>
-      </el-header>
-      <!-- 主体 -->
-      <el-main>Main</el-main>
-      <!-- 底部 -->
-      <el-footer>@CopyRight 20xx-2xxx ???? | For Super Earth !! | xD</el-footer>
-    </el-container>
-  </el-container>
+				</el-sub-menu>
+
+				<!-- 菜单项 线索管理 -->
+				<el-sub-menu index="2">
+					
+					<template #title>
+						<el-icon><DataLine/></el-icon>
+						<span>线索管理</span>
+					</template>
+
+					<el-menu-item index="2-1">
+						<el-icon><DataBoard/></el-icon>线索管理
+					</el-menu-item>
+					<el-menu-item index="2-2">
+						<el-icon><DataAnalysis/></el-icon>线索统计
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 客户管理 -->
+				<el-sub-menu index="3">
+
+					<template #title>
+						<el-icon><UserFilled/></el-icon>
+						<span>客户管理</span>
+					</template>
+
+					<el-menu-item index="3-1">
+						<el-icon><List/></el-icon>客户管理
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 交易管理 -->
+				<el-sub-menu index="4">
+
+					<template #title>
+						<el-icon><WalletFilled/></el-icon>
+						<span>交易管理</span>
+					</template>
+
+					<el-menu-item index="4-1">
+						<el-icon><Present/></el-icon>交易管理
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 产品管理 -->
+				<el-sub-menu index="5">
+
+					<template #title>
+						<el-icon><Box/></el-icon>
+						<span>产品管理</span>
+					</template>
+
+					<el-menu-item index="5-1">
+						<el-icon><Goods/></el-icon>产品管理
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 字典管理 -->
+				<el-sub-menu index="6">
+
+					<template #title>
+						<el-icon><Notebook/></el-icon>
+						<span>字典管理</span>
+					</template>
+
+					<el-menu-item index="6-1">
+						<el-icon><Memo/></el-icon>字典管理
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 用户管理 -->
+				<el-sub-menu index="7">
+
+					<template #title>
+						<el-icon><User/></el-icon>
+						<span>用户管理</span>
+					</template>
+
+					<el-menu-item index="/dashboard/user">
+						<el-icon><Document/></el-icon>用户管理
+					</el-menu-item>
+
+				</el-sub-menu>
+
+				<!-- 菜单项 系统管理 -->
+				<el-sub-menu index="8">
+
+					<template #title>
+						<el-icon><Setting/></el-icon>
+						<span>系统管理</span>
+					</template>
+
+					<el-menu-item index="8-1">
+						<el-icon><Operation/></el-icon>系统管理
+					</el-menu-item>
+
+				</el-sub-menu>
+			</el-menu>
+		</el-aside>
+
+		<!-- 右侧 -->
+		<el-container class="right-container">
+			<!-- 头部 -->
+			<el-header>
+				<el-icon class="fold-icon" @click="changeMenuFolded">
+				<Fold v-if="!menuFolded"/>
+				<Expand v-if="menuFolded"/>
+				</el-icon>
+
+				<div class="dropdown">
+					<el-dropdown :hide-on-click="false" @visible-change="visibleChange">
+						<span class="el-dropdown-link">
+							{{ username }}
+							<el-icon class="el-icon--right" style="vertical-align: middle">
+								<ArrowUp v-if="dropboxVisiable"/>
+								<ArrowDown v-if="!dropboxVisiable" />
+							</el-icon>
+						</span>
+						<template #dropdown>
+							<el-dropdown-menu>
+								<el-dropdown-item>我的资料</el-dropdown-item>
+								<el-dropdown-item>修改密码</el-dropdown-item>
+								<el-dropdown-item divided @click="logout">退出登录</el-dropdown-item>
+							</el-dropdown-menu>
+						</template>
+					</el-dropdown>
+				</div>
+			</el-header>
+
+			<!-- 主体 -->
+			<!-- 使用路由切换 -->
+			<el-main><router-view/></el-main>
+
+			<!-- 底部 -->
+			<el-footer>@CopyRight 20xx-2xxx ???? | For Super Earth !! | xD</el-footer>
+			
+		</el-container>
+	</el-container>
 </template>
 
 <script setup>
