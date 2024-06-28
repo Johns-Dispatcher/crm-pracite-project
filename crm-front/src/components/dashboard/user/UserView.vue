b3200b750da79c60696c0e2e1a944534bfb252a2
johns <johnsun2145@outlook.com>
2024-06-28 23:11:49 +0800

    前端分页显示组件

--- crm-front/src/components/dashboard/user/UserView.vue
+++ crm-front/src/components/dashboard/user/UserView.vue
@@ -1,33 +1,71 @@
 <template>
 	<el-button type="primary">添加用户</el-button>
 	<el-button type="danger">删除所选</el-button>
-	<el-table :data="tableData" style="width: 100%">
-		<el-table-column type="selection" width="55" />
 
-		<el-table-column type="index" width="50" />
+  	<br><br>
 
-		<el-table-column property="loginAct" label="账号名" width="120" />
+	<!-- 用户展示表格 -->
+	<el-table :data="users" style="width: 100%" stripe >
+		<el-table-column type="selection" width="50px" />
 
-		<el-table-column property="name" label="姓名" />
+		<el-table-column type="index" width="50px" :index="startRow"/>
 
-		<el-table-column property="phone" label="电话" />
+		<el-table-column prop="loginAct" label="账号名" show-overflow-tooltip/>
 
-		<el-table-column property="email" label="邮箱" />
+		<el-table-column prop="name" label="姓名" show-overflow-tooltip />
 
-		<el-table-column property="createTime" label="创建时间" />
+		<el-table-column prop="phone" label="电话" show-overflow-tooltip />
+
+		<el-table-column prop="email" label="邮箱" show-overflow-tooltip />
+
+		<el-table-column prop="createTime" label="创建时间" show-overflow-tooltip />
+
+		<el-table-column label="操作">
+			<el-button type="primary">详情</el-button>
+			<el-button type="primary">修改</el-button>
+			<el-button type="danger">删除</el-button>
+		</el-table-column>
   </el-table>
+
+  <br><br>
+
+  <!-- 分页 -->
+  <el-pagination
+    layout="prev, pager, next"
+    :page-size="10"
+    :total="totalCount"
+	@current-change="queryUsers"
+  />
+
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { doGet } from '../../../http/httpRequestUtils';
 
-const users = undefined
+/* == 数据 == */
+
+const users = ref([])
+const totalCount = ref(0)
+const startRow = ref(1)
+
+/* == 函数 == */
+
+function queryUsers(current) {
+	doGet("/api/user/page/" + current, {}).then(response => {
+		console.log(response)
+		if (response.data.code === 200) {
+			users.value = response.data.data.list
+			totalCount.value = response.data.data.total
+			startRow.value = response.data.data.startRow
+		}
+	})
+}
+
+/* == 钩子函数 == */
 
 onMounted(() => {
-	doGet("/api/user", {}).then(response => {
-		console.log(response)
-	})
+	queryUsers(1)
 })
 </script>
 
