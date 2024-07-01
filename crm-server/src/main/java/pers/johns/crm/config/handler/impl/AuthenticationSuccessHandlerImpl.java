70a4bd9b8956fdfa85780608e029594484496226
johns <johnsun2145@outlook.com>
2024-07-01 22:55:59 +0800

    修正

--- crm-server/src/main/java/pers/johns/crm/config/handler/impl/AuthenticationSuccessHandlerImpl.java
+++ crm-server/src/main/java/pers/johns/crm/config/handler/impl/AuthenticationSuccessHandlerImpl.java
@@ -1,6 +1,5 @@
 package pers.johns.crm.config.handler.impl;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
