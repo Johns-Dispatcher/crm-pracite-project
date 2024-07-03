7fd9703d63abfda6c364a3b9b4e247b896877bf5
johns <johnsun2145@outlook.com>
2024-07-03 20:47:24 +0800

    增加添加活动接口、修改活动接口、获取活动信息接口

--- crm-server/src/main/java/pers/johns/crm/web/controller/ActivityController.java
+++ crm-server/src/main/java/pers/johns/crm/web/controller/ActivityController.java
@@ -3,8 +3,11 @@ package pers.johns.crm.web.controller;
 import com.github.pagehelper.PageInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pers.johns.crm.model.vo.HttpResult;
+import pers.johns.crm.model.vo.SecurityUser;
+import pers.johns.crm.model.vo.ViewActivity;
 import pers.johns.crm.query.ActivitySearchQuery;
 import pers.johns.crm.service.ActivityService;
 
@@ -39,14 +42,63 @@ public class ActivityController {
         return HttpResult.OK("获取活动成功", activitiesByPage);
     }
 
+    /**
+     * 获取当前负责活动的用户，只获取有活动的用户
+     * @return 含有用户名信息的响应结果
+     */
     @GetMapping("/owner")
     public HttpResult getActivityOwners() {
-        return HttpResult.OK("获取活动拥有者成功", activityService.getActivityOwners());
+        return HttpResult.OK("获取活动负责人成功", activityService.getActivityOwners());
     }
 
+    /**
+     * 按照条件进行搜索，同时进行分页查询<br/>
+     * （似乎可以与分页产线合并...）
+     * @param activitySearchQuery 筛选信息
+     * @return 含有分页信息的响应结果对象
+     */
     @PostMapping("/search")
     public HttpResult searchActivities(@RequestBody ActivitySearchQuery activitySearchQuery) {
         PageInfo<Object> pageInfo = activityService.searchActivitiesByPage(activitySearchQuery);
         return HttpResult.OK("搜索查询活动成功", pageInfo);
+    }
+
+    /**
+     * 新增活动信息
+     * @param viewActivity 前端提供的活动信息
+     * @param authentication 当前登录的认证信息
+     * @return 含有成功信息的响应结果对象
+     */
+    @PostMapping("/")
+    public HttpResult addActivity(@RequestBody ViewActivity viewActivity, Authentication authentication) {
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        viewActivity.setCreateBy(securityUser.getId());
+        viewActivity.setEditBy(securityUser.getId());
+
+        return HttpResult.OK("添加活动成功", activityService.addActivity(viewActivity));
+    }
+
+    /**
+     * 通过 id 获取指定活动信息
+     * @param id 活动 id
+     * @return 含有活动信息响应结果对象
+     */
+    @GetMapping("/{id}")
+    public HttpResult getActivity(@PathVariable("id") Integer id) {
+        return HttpResult.OK("获取指定活动信息", activityService.getActivity(id));
+    }
+
+    /**
+     * 修改活动信息
+     * @param viewActivity 活动信息
+     * @param authentication 登录的认证信息
+     * @return 含有成功信息的响应结果对象
+     */
+    @PutMapping("/")
+    public HttpResult editActivity(@RequestBody ViewActivity viewActivity, Authentication authentication) {
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        viewActivity.setEditBy(securityUser.getId());
+
+        return HttpResult.OK("修改活动成功", activityService.editActivity(viewActivity));
     }
 }
