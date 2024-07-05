41303e09022a6eb9e5c41f388047cfbdbd743e36
johns <johnsun2145@outlook.com>
2024-07-06 00:33:00 +0800

    为 EasyExcel 提供字段对应方式

--- crm-server/src/main/java/pers/johns/crm/model/vo/ViewClue.java
+++ crm-server/src/main/java/pers/johns/crm/model/vo/ViewClue.java
@@ -1,11 +1,13 @@
 package pers.johns.crm.model.vo;
 
+import com.alibaba.excel.annotation.ExcelProperty;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import pers.johns.crm.model.vo.constant.ClueExcelHeader;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -29,28 +31,47 @@ import java.time.LocalDateTime;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class ViewClue implements Serializable {
     private Integer id;
+    @ExcelProperty(ClueExcelHeader.OWNER_ID)
     private Integer ownerId;
     private String owner;
+    @ExcelProperty(ClueExcelHeader.ACTIVITY_ID)
     private Integer activityId;
     private String activity;
+    @ExcelProperty(ClueExcelHeader.FULL_NAME)
     private String fullName;
+    @ExcelProperty(ClueExcelHeader.APPELLATION)
     private String appellation;
+    @ExcelProperty(ClueExcelHeader.PHONE)
     private String phone;
+    @ExcelProperty(ClueExcelHeader.WECHAT)
     private String wechat;
+    @ExcelProperty(ClueExcelHeader.QQ)
     private String qq;
+    @ExcelProperty(ClueExcelHeader.EMAIL)
     private String email;
+    @ExcelProperty(ClueExcelHeader.AGE)
     private Integer age;
+    @ExcelProperty(ClueExcelHeader.JOB)
     private String job;
+    @ExcelProperty(ClueExcelHeader.YEAR_INCOME)
     private Double yearIncome;
+    @ExcelProperty(ClueExcelHeader.ADDRESS)
     private String address;
+    @ExcelProperty(ClueExcelHeader.NEED_LOAN)
     private String needLoan;
+    @ExcelProperty(ClueExcelHeader.INTENTION_STATE)
     private String intentionState;
     private Integer intentionProduct;
+    @ExcelProperty(ClueExcelHeader.PRODUCT_NAME)
     private String productName;
+    @ExcelProperty(ClueExcelHeader.STATE)
     private String state;
+    @ExcelProperty(ClueExcelHeader.SOURCE)
     private String source;
+    @ExcelProperty(ClueExcelHeader.DESCRIPTION)
     private String description;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    @ExcelProperty(ClueExcelHeader.NEXT_CONTACT_TIME)
     private LocalDateTime nextContactTime;
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private LocalDateTime createTime;
