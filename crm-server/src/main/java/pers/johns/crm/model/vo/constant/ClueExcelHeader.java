40c5973d2eee69ee42bd048e9c8fd442545dace3
johns <johnsun2145@outlook.com>
2024-07-06 00:35:23 +0800

    用于对应 Excel 表头的字段名常量

---
+++ crm-server/src/main/java/pers/johns/crm/model/vo/constant/ClueExcelHeader.java
@@ -0,0 +1,36 @@
+package pers.johns.crm.model.vo.constant;
+
+/**
+ * ClassName    : ClueExcelHeader
+ * <br/>
+ * Description  : 线索 Excel 头部常量
+ * <br/>
+ * CreateTime   : 2024/7/5 20:50
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+public class ClueExcelHeader {
+    private ClueExcelHeader() {}
+
+    public static final String OWNER_ID = "负责人";
+    public static final String ACTIVITY_ID = "所属活动";
+    public static final String FULL_NAME = "姓名";
+    public static final String APPELLATION = "称呼";
+    public static final String PHONE = "手机号";
+    public static final String WECHAT = "微信号";
+    public static final String QQ = "QQ号";
+    public static final String EMAIL = "邮箱";
+    public static final String AGE = "年龄";
+    public static final String JOB = "职业";
+    public static final String YEAR_INCOME = "年收入";
+    public static final String ADDRESS = "地址";
+    public static final String NEED_LOAN = "是否贷款";
+    public static final String INTENTION_STATE = "意向状态";
+    public static final String PRODUCT_NAME = "意向产品";
+    public static final String STATE = "线索状态";
+    public static final String SOURCE = "线索来源";
+    public static final String DESCRIPTION = "线索描述";
+    public static final String NEXT_CONTACT_TIME = "下次联系时间";
+}
