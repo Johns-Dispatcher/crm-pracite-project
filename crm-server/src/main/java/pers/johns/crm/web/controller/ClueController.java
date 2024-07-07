e9c79d8759035f93183fc9739395ceaa819b7c65
johns <johnsun2145@outlook.com>
2024-07-07 23:05:16 +0800

    提供增加线索接口、查询电话接口

--- crm-server/src/main/java/pers/johns/crm/web/controller/ClueController.java
+++ crm-server/src/main/java/pers/johns/crm/web/controller/ClueController.java
@@ -3,13 +3,17 @@ package pers.johns.crm.web.controller;
 import com.github.pagehelper.PageInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pers.johns.crm.model.vo.HttpResult;
+import pers.johns.crm.model.vo.SecurityUser;
+import pers.johns.crm.model.vo.ViewClue;
 import pers.johns.crm.query.ClueQuery;
 import pers.johns.crm.service.ClueService;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 /**
  * ClassName    : ClueController
@@ -43,9 +47,43 @@ public class ClueController {
         return HttpResult.OK("获取线索成功", pageInfo);
     }
 
+    /**
+     * 以 Excel 文件批量导入数据库
+     * @param file Excel 文件
+     * @return 导入成功的响应结果信息
+     * @throws IOException I/O 异常
+     */
     @PostMapping("/importExcel")
     public HttpResult uploadExcel(@RequestParam("file") MultipartFile file) throws IOException {
 
         return HttpResult.OK("Excel 导入成功", clueService.importExcel(file));
+    }
+
+    /**
+     * 添加线索
+     * @param viewClue 线索信息
+     * @param authentication 认证信息
+     * @return 添加成功的响应结果信息
+     */
+    @PostMapping("/")
+    public HttpResult addClue(@RequestBody ViewClue viewClue, Authentication authentication) {
+        SecurityUser securityUser = (SecurityUser) authentication.getPrincipal();
+        viewClue.setOwnerId(securityUser.getId());
+        viewClue.setCreateBy(securityUser.getId());
+        viewClue.setEditBy(securityUser.getId());
+        viewClue.setCreateTime(LocalDateTime.now());
+        viewClue.setEditTime(LocalDateTime.now());
+
+        return HttpResult.OK("添加成功", clueService.addClue(viewClue));
+    }
+
+    /**
+     * 验证当前手机号是否存在
+     * @param phone 手机号
+     * @return 查询验证结果
+     */
+    @GetMapping("/phone/{phone}")
+    public HttpResult checkPhone(@PathVariable("phone") String phone) {
+        return HttpResult.OK("查询结果", clueService.checkPhoneExisted(phone));
     }
 }
