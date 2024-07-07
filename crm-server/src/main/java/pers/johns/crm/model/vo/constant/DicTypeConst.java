852877a3030b176d507232182060164e04ca28fb
johns <johnsun2145@outlook.com>
2024-07-07 23:22:19 +0800

    字典类型的常量类

---
+++ crm-server/src/main/java/pers/johns/crm/model/vo/constant/DicTypeConst.java
@@ -0,0 +1,28 @@
+package pers.johns.crm.model.vo.constant;
+
+/**
+ * ClassName    : DicTypeConst
+ * <br/>
+ * Description  : 字典类型常量类
+ * <br/>
+ * CreateTime   : 2024/7/6 23:12
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+public class DicTypeConst {
+    public static final String SEX = "sex";
+    public static final String APPELLATION = "appellation";
+    public static final String CLUE_STATE = "clueState";
+    public static final String RETURN_PRIORITY = "returnPriority";
+    public static final String RETURN_STATE = "returnState";
+    public static final String SOURCE = "source";
+    public static final String STAGE = "stage";
+    public static final String TRANSACTION_TYPE = "transactionType";
+    public static final String INTENTION_STATE = "intentionState";
+    public static final String NEED_LOAN = "needLoan";
+    public static final String EDUCATIONAL = "educational";
+    public static final String USER_STATE = "userState";
+    public static final String NOTE_WAY = "noteWay";
+}
