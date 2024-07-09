bb54af589d55ded5420ac561096d1dc22499d866
johns <johnsun2145@outlook.com>
2024-07-09 08:53:33 +0800

    视图客户对象，用于给前端传输数据

---
+++ crm-server/src/main/java/pers/johns/crm/model/vo/ViewCustomer.java
@@ -0,0 +1,47 @@
+package pers.johns.crm.model.vo;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * ClassName    : ViewCustomer
+ * <br/>
+ * Description  : 视图客户对象
+ * <br/>
+ * CreateTime   : 2024/7/8 21:14
+ * <br/>
+ *
+ * @author : JohnS
+ * @version : 1.0
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+public class ViewCustomer implements Serializable {
+    private Integer id;
+    private Integer clueId;
+    private String fullName;
+    private Integer product;
+    private String productName;
+    private String description;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime nextContactTime;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime createTime;
+    private Integer createBy;
+    private String creator;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private LocalDateTime editTime;
+    private Integer editBy;
+    private String editor;
+}
